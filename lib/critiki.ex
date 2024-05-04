789b3c6815d8643a39ecd6367abd72a08f8e2ec2
Eskil Heyn Olsen <eskil@eskil.org>
2024-05-04 13:36:09 -0700

    Initial commit

---
+++ lib/critiki.ex
@@ -0,0 +1,18 @@
+defmodule Critiki do
+  @moduledoc """
+  Documentation for `Critiki`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Critiki.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/critiki_test.exs
@@ -0,0 +1,8 @@
+defmodule CritikiTest do
+  use ExUnit.Case
+  doctest Critiki
+
+  test "greets the world" do
+    assert Critiki.hello() == :world
+  end
+end
